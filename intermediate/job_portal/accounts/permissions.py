3cebec8befd23b07889fcda1742ed7311b57d2fb
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-02-28 13:34:32 +0530

    feat: add custom admin role permission

--- intermediate/job_portal/accounts/permissions.py
+++ intermediate/job_portal/accounts/permissions.py
@@ -14,3 +14,12 @@ class IsCandidate(BasePermission):
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return request.user.role == "ADMIN"
+
+
+class IsAdminUserRole(BasePermission):
+    """
+    Allows access only to users with ADMIN role.
+    """
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == "ADMIN"
