bf7cd24cd408f0a3b58f52f725b78eee31a14935
AHTHneeuhl <ahthneeuhl@gmail.com>
2026-02-28 13:43:52 +0530

    feat: implement admin analytics aggregation endpoint

--- intermediate/job_portal/jobs/views.py
+++ intermediate/job_portal/jobs/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Job
